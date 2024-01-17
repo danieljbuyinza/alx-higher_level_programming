1dcc1a27854c09911a0654d1a6f7a365d83064fb
danieljbuyinza <danieljbuyinza@gmail.com>
2024-01-17 02:36:32 -0800

    Fix if conditions.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1])
-print("Last digit of", number, "is", last_digit * -1 if number < 0 else last_digit, end="")
-if last_digit > 5:
-    print(" and is greater than 5")
-elif last_digit == 0:
-    print(" and is 0")
-else:
+if number > 0:
+    print("Last digit of", number, "is", last_digit, end="")
+    if last_digit > 5:
+        print(" and is greater than 5")
+    elif last_digit == 0:
+        print(" and is 0")
+
+if number < 0:
+    print("Last digit of", number, "is", last_digit * -1, end="")
     print(" and is less than 6 and not 0")
