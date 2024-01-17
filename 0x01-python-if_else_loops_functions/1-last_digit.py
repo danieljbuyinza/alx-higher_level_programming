e03b9ecdebb9c7ab5dddc22ad6b16576366f45ed
danieljbuyinza <danieljbuyinza@gmail.com>
2024-01-17 02:05:50 -0800

    Restructure if conditions.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1])
 print("Last digit of", number, "is", last_digit)
-print("and is greater than 5" if last_digit > 5)
-print("and is 0" if last_digit == 0)
-print("and is less than 6 and not 0" if last_digit < 6 and not 0)
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
