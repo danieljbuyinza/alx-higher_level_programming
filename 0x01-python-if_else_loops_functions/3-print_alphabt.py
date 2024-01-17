5a3ec01fb981c651b6c5bb2ab565f3276c79d681
danieljbuyinza <danieljbuyinza@gmail.com>
2024-01-17 04:21:59 -0800

    Fix typos.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for character in range(97, 123):
-    if(chr(character) != 'e') and chr(character) != 'q'):
+    if (chr(character) != 'e') and (chr(character) != 'q'):
         print(chr(character).format(character), end="")
